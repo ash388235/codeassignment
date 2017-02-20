3e8727074c8c024c9b9bae5b80763b73090402de
ash.388235@gmail.com <kaival1911>
2017-02-20 13:11:10 -0500

    unit test added for exception

--- codetask/src/test/java/pimco/com/codetask/MyFilterIteratorTest.java
+++ codetask/src/test/java/pimco/com/codetask/MyFilterIteratorTest.java
@@ -3,6 +3,7 @@ package pimco.com.codetask;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.junit.Test;
 
@@ -36,7 +37,19 @@ public class MyFilterIteratorTest {
 		for (int i = 0; i < filteredList.size(); i++) {
 			Assert.assertEquals(filteredList.get(i), expectedFilteredList.get(i));
 		}
-
 	}
+	
+	/*Test to make sure next method behaviour is same as original iterator*/
+	@Test(expected=NoSuchElementException.class)
+	public void myFilterIteratorExceptionTest() {
+		List<Double> list = new ArrayList<Double>();
+		
+		list.add(-1d);
+		Iterator<Double> itr = list.iterator();
+		FilteringIterator<Double> profitLossFiteringIterator = new FilteringIterator<Double>(new ProfitLossTestImpl(),itr);
 
+		profitLossFiteringIterator.next();
+	}
+	
+	
 }
