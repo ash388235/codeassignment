87e5fff42be2bc7aeb4fa5ef1004b37bef446240
ashishpatel <kaival1911>
2017-02-20 11:14:49 -0500

    first commit

---
+++ codetask/src/main/java/pimco/com/codetask/App.java
@@ -0,0 +1,13 @@
+package pimco.com.codetask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
